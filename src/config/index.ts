21ba5014d39244271f23bc8895eca9a454cf62d7
Majed Hasan <mdmajedhasan599@gmail.com>
2026-05-11 00:54:58 +0600

    Fixes the problem of the jwt in the /config/index.ts file

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,13 +7,14 @@ export default {
   jwt: {
     accessSecret: process.env.JWT_ACCESS_SECRET || "access",
     refreshSecret: process.env.JWT_REFRESH_SECRET || "refresh",
-    accessExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN || "15m",
-    refreshExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN || "7d",
+    // Cast these to any or string to satisfy the jwt.sign overload
+    accessExpiresIn: (process.env.ACCESS_TOKEN_EXPIRES_IN || "15m") as any,
+    refreshExpiresIn: (process.env.REFRESH_TOKEN_EXPIRES_IN || "7d") as any,
   },
   verification: {
-    expiresIn: process.env.VERIFY_EXPIRES_IN || "1d",
+    expiresIn: (process.env.VERIFY_EXPIRES_IN || "1d") as any,
   },
   reset: {
-    expiresIn: process.env.RESET_EXPIRES_IN || "1h",
+    expiresIn: (process.env.RESET_EXPIRES_IN || "1h") as any,
   },
 };
